4c40e4b3b19278648920c5dc1cee6205823390d6
nweilersg <44669670+nweilersg@users.noreply.github.com>
2025-02-20 08:19:18 +0800

    Update contract.py

--- contract.py
+++ contract.py
@@ -59,10 +59,10 @@ if st.button('Send'):
         st.warning('Source contract document2?')
         st.stop()
         
-    #client = OpenAI(api_key=api_key)
-    client = OpenAI(api_key=api_key,base_url="https://api.deepseek.com")
+    client = OpenAI(api_key=api_key)
+    #client = OpenAI(api_key=api_key,base_url="https://api.deepseek.com")
     response = client.chat.completions.create(
-        model='deepseek-chat',
+        model='gpt-4o',
         temperature=0.0,
         max_tokens=500,
         messages=[msg],
